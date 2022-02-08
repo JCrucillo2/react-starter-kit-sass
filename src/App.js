1e52e62093d2478f00911a973506a6501bf3481e
Junnel Crucillo <71023403+JCrucillo2@users.noreply.github.com>
2022-02-08 09:47:59 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// Function => Component JSX(markup/data)
+// Components ***** must start from a single node
+// element
+// <></> react fragment empty html element jsx
+function App() {
+    return (
+        <>
+            <header>
+                <h1>React Starter Kit</h1>
+            </header>
+        </>
+    );
+}
+
+export default App;
